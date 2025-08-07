5fec952a26a1c43276820631f3da6781d164d36a
Nusademon <136218500+Nusademon@users.noreply.github.com>
2025-08-07 18:16:35 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ const chalk = require('chalk');
 //~~~~~~~~~~~~< GLOBAL SETTINGS >~~~~~~~~~~~~\\
 
 global.owner = ['6282113821188'] //['628','628'] 2 owner atau lebih
-global.author = 'Nazedev'
+global.author = 'Nazededv'
 global.botname = 'Hitori Bot'
 global.packname = 'Bot WhatsApp'
 global.listprefix = ['+','!','.']
@@ -90,3 +90,4 @@ fs.watchFile(file, () => {
 	delete require.cache[file]
 	require(file)
 });
+
